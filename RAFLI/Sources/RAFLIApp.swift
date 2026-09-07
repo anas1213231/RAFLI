ebbde52b202bdd590d574b7bff78d7f674059c3c
anas1213231 <anas2007250@gmail.com>
2026-09-07 19:57:51 +0300

    Use rebuilt RAFLI root interface

--- RAFLI/Sources/RAFLIApp.swift
+++ RAFLI/Sources/RAFLIApp.swift
@@ -3,6 +3,8 @@ import SwiftUI
 @main
 struct RAFLIApp: App {
     var body: some Scene {
-        WindowGroup { ContentView() }
+        WindowGroup {
+            RAFLIRootView()
+        }
     }
 }
